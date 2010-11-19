947b0f5e26a8a5ec8207ed977e19b7a5e711771f
Christopher Denter <dennda@the-space-station.com>
2010-11-19 16:33:56 +0100

    Fix autofocus on mouse enter

--- rc.lua
+++ rc.lua
@@ -302,9 +302,6 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    { rule = { class = "empathy" },
-      properties = { floating = true,
-      		     tag[1][5] } },
     -- Set Firefox to always map on tags number 2 of screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { tag = tags[1][2] } },
